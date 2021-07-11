7ada21834842f2d1d11ebe0f37aa2d5a2ce8740d
Dat Nguyen Quy <datnguyenquy94@gmail.com>
2021-07-11 09:42:10 +0700

    change extract-files.sh and setup-makefiles.sh to LineageOS's files

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #
 # Copyright (C) 2020 The LineageOS Project
-# Copyright (C) 2021 Syberia Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -17,9 +16,9 @@ INITIAL_COPYRIGHT_YEAR=2021
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-SYBERIA_ROOT="${MY_DIR}/../../.."
+LINEAGE_ROOT="${MY_DIR}/../../.."
 
-HELPER="${SYBERIA_ROOT}/vendor/syberia/build/tools/extract_utils.sh"
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -27,7 +26,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE}" "${VENDOR}" "${SYBERIA_ROOT}" true
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
 write_headers "alioth thyme"
@@ -38,7 +37,7 @@ write_makefiles "${MY_DIR}/proprietary-files.txt" true
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${SYBERIA_ROOT}" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
 
     # Copyright headers and guards
     write_headers
